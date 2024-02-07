17749076a04979f4d78c3f2c4eb1344f1f1cfe7b
Marija Milkovska <m.milkovska12@gmail.com>
2024-02-07 01:40:50 +0100

    Initial Commit

---
+++ RestaurantMenu/RestaurantMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantMenuApp.swift
+//  RestaurantMenu
+//
+//  Created by Marija Milkovska on 7.2.24.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
